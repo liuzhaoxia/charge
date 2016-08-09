ddc061ab1ca6367d2702ac0bb663585fd6e10173
xujie3949 <xujie3949@navinfo.com>
2016-08-09 17:09:08 +0800

    完成登陆界面，添加eslint检查

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,35 +1,39 @@
 /**
  * Created by liwanchong on 2016/8/2.
  */
-const helper = {
-    bindMethod: (obj)=> {
-        const members = Object.getOwnPropertyNames(obj.__proto__);
-        for (let member of members) {
-            if (typeof(obj[member]) === 'function') {
-                if (!helper.isReactLifeCycleMethod(member)) {
-                    obj[member] = obj[member].bind(obj);
-                }
-            }
-        }
-    },
 
-    isReactLifeCycleMethod: (methodName)=> {
-        switch (methodName) {
-            case 'getInitialState':
-            case 'getDefaultProps':
-            case 'componentWillMount':
-            case 'componentDidMount':
-            case 'componentWillReceiveProps':
-            case 'shouldComponentUpdate':
-            case 'componentWillUpdate':
-            case 'componentDidUpdate':
-            case 'componentWillUnmount':
-            case 'constructor':
-                return true;
-            default :
-                return false;
+const Helper = {
+  bindMethod: (obj) => {
+    const prototype = Object.getPrototypeOf(obj);
+    const members = Object.getOwnPropertyNames(prototype);
+    for (const member of members) {
+      if (typeof (obj[member]) === 'function') {
+        if (!Helper.isReactLifeCycleMethod(member)) {
+          /* eslint-disable no-param-reassign */
+          obj[member] = obj[member].bind(obj);
+          /* eslint-disable no-param-reassign */
         }
+      }
     }
+  },
+
+  isReactLifeCycleMethod: (methodName) => {
+    switch (methodName) {
+      case 'getInitialState':
+      case 'getDefaultProps':
+      case 'componentWillMount':
+      case 'componentDidMount':
+      case 'componentWillReceiveProps':
+      case 'shouldComponentUpdate':
+      case 'componentWillUpdate':
+      case 'componentDidUpdate':
+      case 'componentWillUnmount':
+      case 'constructor':
+        return true;
+      default :
+        return false;
+    }
+  },
 };
 
-export default helper;
+export default Helper;
