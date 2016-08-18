8826e34ad919be3e6f2d882d170de23dc3eabfec
wangtun <wangtun@navinfo.com>
2016-08-18 15:20:11 +0800

    解决冲突

--- src/containers/android/app.js
+++ src/containers/android/app.js
@@ -7,53 +7,6 @@ import {
   Image,
   Text,
 } from 'react-native';
-<<<<<<< HEAD
-
-import {Provider, connect} from 'react-redux';
-import Choose from './../../containers/android/Choose';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Router, Scene, Modal,ActionConst } from 'react-native-router-flux';
-import Main from './../../containers/android/Main';
-import About from './../../containers/android/About';
-import routeReducerCreator from './../../reducers/routeReducerCreator';
-import store from './../../store/store';
-import DetailInfo from '../../containers/android/Detail';
-
-
-
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        //helper.bindMethod(this);
-    }
-
-    render() {
-        return (
-            <Provider store={store}>
-                <Router createReducer={routeReducerCreator}>
-                    <Scene key="modal" component={Modal}>
-                        <Scene key="root" hideNavBar hideTabBar>
-                            <Scene key="mainModule"  direction="horizontal" initial={true}>
-                                <Scene key="main" component={Main} title="main"
-                                       hideNavBar/>
-                                <Scene key="DetailInfo" component={DetailInfo} title="DetailInfo"
-                                hideNavBar/>
-                                <Scene key="Choose" component={Choose} title="Choose"
-                                       hideNavBar/>
-                            </Scene>
-
-                            <Scene direction="horizontal" key="About" component={About} schema="modal" title="About"
-                                   hideNavBar/>
-
-
-                        </Scene>
-                        <Scene key="error" component={Error}/>
-                    </Scene>
-                </Router>
-            </Provider>
-        );
-    }
-=======
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider, connect } from 'react-redux';
 import { Router, Scene, Modal } from 'react-native-router-flux';
@@ -106,7 +59,6 @@ class App extends React.Component {
       </Provider>
     );
   }
->>>>>>> charge/master
 }
 
 export default App;
