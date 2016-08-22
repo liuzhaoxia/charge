03b4d4910e8534eea0cd9fae363fd7e5895c2993
wangtun <wangtun@navinfo.com>
2016-08-22 13:51:05 +0800

    修改主页格式

--- src/containers/android/app.js
+++ src/containers/android/app.js
@@ -27,27 +27,21 @@ class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
-            <Router createReducer={routeReducerCreator}>
-            <Scene key="modal" component={Modal}>
-            <Scene key="root" hideNavBar hideTabBar>
-
-        <Scene key="mainModule"  direction="horizontal" initial={true}>
-            <Scene key="main" component={Main} title="main"
-        hideNavBar/>
-        <Scene key="DetailInfo" component={DetailInfo} title="DetailInfo"
-        hideNavBar/>
-        </Scene>
-        <Scene key="Choose" component={Choose} title="Choose" hideNavBar />
-        <Scene direction="horizontal" key="About" component={About} schema="modal" title="About"
-        hideNavBar/>
-
-
-        </Scene>
-        <Scene key="error" component={Error}/>
-            </Scene>
-            </Router>
+                <Router createReducer={routeReducerCreator}>
+                        <Scene key="modal" component={Modal}>
+                            <Scene key="root" hideNavBar hideTabBar>
+                                <Scene key="mainModule"  direction="horizontal" initial={true}>
+                                <Scene key="main" component={Main} title="main" hideNavBar/>
+                                <Scene key="DetailInfo" component={DetailInfo} title="DetailInfo" hideNavBar/>
+                            </Scene>
+                            <Scene key="Choose" component={Choose} title="Choose" hideNavBar />
+                            <Scene direction="horizontal" key="About" component={About} schema="modal" title="About" hideNavBar/>
+                       </Scene>
+                       <Scene key="error" component={Error}/>
+                    </Scene>
+                </Router>
             </Provider>
-    );
+        );
     }
 }
 
