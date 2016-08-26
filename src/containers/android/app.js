ddfad8ae826592c77b148ed909cd9eeebe7bc726
z50902677 <18697479319@163.com>
2016-08-26 17:50:10 +0800

    修改

--- src/containers/android/app.js
+++ src/containers/android/app.js
@@ -6,6 +6,8 @@ import {
     View,
     Image,
     Text,
+    StyleSheet,
+    TextInput,
 } from 'react-native';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Scene, Modal,ActionConst } from 'react-native-router-flux';
@@ -26,99 +28,128 @@ import Main from './../../containers/android/Main';
 import SearchList from '../../containers/android/SearchList';
 import { Global } from '../../Global';
 import imageViewPage from '../../containers/android/imageViewPager';
-class App extends React.Component {
-  componentWillUnmount() {
-    store.get('appState')
-      .then(res => {
-        if (res) {
-          console.log("---");
-          console.log(Global.appState);
-          console.log("---");
-          store.update('appState', Global.appState);
-          return;
-        }
-      console.log("------");
-        console.log(Global.appState);
-    console.log("------");
-        store.save('appState', Global.appState);
-      })
-      .catch(error => {
-          store.save('appState', Global.appState);
-    console.log("----------");
-          console.error(error.message);
-    console.log("----------");
-      });
-  }
-    test1(){
-        alert(111);
+const styles = StyleSheet.create({
+    textInput: {
+        color: 'black',
+        flex:1,
+        height:40
+    },
+    container:{
+        width:250,
+        marginLeft:60,
+        borderWidth:1,
+        borderColor:'red',
+        flexDirection: 'row',
+        marginTop:5
     }
-  render() {
-    return (
+});
+class App extends React.Component {
+    componentWillUnmount() {
+        store.get('appState')
+            .then(res => {
+                if (res) {
+                    console.log("---");
+                    console.log(Global.appState);
+                    console.log("---");
+                    store.update('appState', Global.appState);
+                    return;
+                }
+                console.log("------");
+                console.log(Global.appState);
+                console.log("------");
+                store.save('appState', Global.appState);
+            })
+            .catch(error => {
+                store.save('appState', Global.appState);
+                console.log("----------");
+                console.error(error.message);
+                console.log("----------");
+            });
+    }
 
-      <Provider store={ReduxStore}>
-        <Router createReducer={routeReducerCreator}>
-          <Scene key="modal" component={Modal}>
-            <Scene key="root">
-              <Scene key="start" component={Start} title="Start" hideNavBar hideTabBar initial />
-              <Scene key="login" component={Login} title="Login" hideNavBar />
-              <Scene key="mainModule" direction="horizontal">
-                <Scene key="main" component={Main} title="Main" hideNavBar />
-                <Scene
-                  key="shellsDetail"
-                  component={ShellsDetail}
-                  title="shellsDetail"
-                  hideNavBar
-                />
-                <Scene
-                  key="DetailInfo"
-                  component={DetailInfo}
-                  title="DetailInfo"
-                  hideNavBar
-                />
-              <Scene
-                  key="imageViewPage"
-                  component={imageViewPage}
-                  title="imageViewPage"
-                  hideNavBar/>
-              </Scene>
-                <Scene
-                    key="Choose"
-                    component={Choose}
-                    title="个人定制"
-                    rightTitle="重置"
-                    onRight={this.test1}
-                    hideNavBar={false}
-                />
-              <Scene
-                key="SearchList"
-                component={SearchList}
-                title="搜索"
-                hideNavBar
-              />
-              <Scene
-                key="About" component={About}
-                title="关于"
-                hideNavBar={false}
-              />
-              <Scene
-                  key="HelpView"
-                  component={HelpView}
-                  title="帮助"
-                  hideNavBar={false}
-              />
-               <Scene
-                    key="ChargeView"
-                    component={ChargeView}
-                    title="桩家视界"
-                    hideNavBar={false}
-               />
-            </Scene>
-            <Scene key="error" component={Error} />
-          </Scene>
-        </Router>
-      </Provider>
-    );
-  }
+    test1() {
+        console.log(2222);
+    }
+    searchCharge(){
+
+    }
+    title() {
+        return (
+            <View style={styles.container}>
+                <TextInput placeholder="搜索地点" placeholderTextColor='black' style={styles.textInput}
+                           underlineColorAndroid='transparent' autoFocus={true} onChange={this.searchCharge}>
+                </TextInput>
+            </View>
+        )
+    }
+
+    render() {
+        return (
+
+            <Provider store={ReduxStore}>
+                <Router createReducer={routeReducerCreator}>
+                    <Scene key="modal" component={Modal}>
+                        <Scene key="root">
+                            <Scene key="start" component={Start} title="Start" hideNavBar hideTabBar initial/>
+                            <Scene key="login" component={Login} title="Login" hideNavBar/>
+                            <Scene key="mainModule" direction="horizontal">
+                                <Scene key="main" component={Main} title="Main" hideNavBar/>
+                                <Scene
+                                    key="shellsDetail"
+                                    component={ShellsDetail}
+                                    title="shellsDetail"
+                                    hideNavBar
+                                />
+                                <Scene
+                                    key="DetailInfo"
+                                    component={DetailInfo}
+                                    title="DetailInfo"
+                                    hideNavBar
+                                />
+                                <Scene
+                                    key="imageViewPage"
+                                    component={imageViewPage}
+                                    title="imageViewPage"
+                                    hideNavBar/>
+                            </Scene>
+                            <Scene
+                                key="Choose"
+                                component={Choose}
+                                title="个人定制"
+                                rightTitle="重置"
+                                onRight={this.test1}
+                                hideNavBar={false}
+                            />
+                            <Scene
+                                key="SearchList"
+                                component={SearchList}
+                                hideNavBar={false}
+                                renderTitle={this.title}
+                            />
+                            <Scene
+                                key="About" component={About}
+                                title="关于"
+                                hideNavBar={false}
+                            />
+                            <Scene
+                                key="HelpView"
+                                component={HelpView}
+                                title="帮助"
+                                hideNavBar={false}
+                            />
+                            <Scene
+                                key="ChargeView"
+                                component={ChargeView}
+                                title="桩家视界"
+                                hideNavBar={false}
+                            />
+                        </Scene>
+                        <Scene key="error" component={Error}/>
+                    </Scene>
+                </Router>
+            </Provider>
+        );
+    }
 
 }
 
